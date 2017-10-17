05270c251a383822b0cf720adaf257dd840d8659
Rudhi Sasmito <rudhisasmito@gmail.com>
2017-10-17 10:10:42 +0700

    files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	/*-----------------------------------/
+	/*	TOP NAVIGATION AND LAYOUT
+	/*----------------------------------*/
+
+	$('.btn-toggle-fullwidth').on('click', function() {
+		if(!$('body').hasClass('layout-fullwidth')) {
+			$('body').addClass('layout-fullwidth');
+
+		} else {
+			$('body').removeClass('layout-fullwidth');
+			$('body').removeClass('layout-default'); // also remove default behaviour if set
+		}
+
+	});
+
+
+});
